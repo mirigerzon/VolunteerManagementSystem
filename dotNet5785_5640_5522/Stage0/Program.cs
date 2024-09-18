ddcdf10340ea0f7f5ecf5b7cf506548e75de6af3
mirigerzon <A0583286530@gmail.com>
2024-09-18 23:09:13 +0300

    Initial code + wellcome functiion

---
+++ dotNet5785_5640_5522/Stage0/Program.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    static void Main(string[] args)
+    {
+        Welcome5640();
+        wellcome5522();
+        Console.ReadKey();
+    }
+    private static void Welcome5640()
+    {
+        Console.WriteLine("Enter your name");
+        string userName = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console", userName);
+    }
+
+    static partial void wellcome5522();
+}
