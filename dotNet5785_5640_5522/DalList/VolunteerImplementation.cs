76b875295b827b401ee8a76d946bd818730bc65a
NoyatAvigail <avigaildvd@gmail.com>
2024-11-25 15:28:38 +0200

    step 10

--- dotNet5785_5640_5522/DalList/VolunteerImplementation.cs
+++ dotNet5785_5640_5522/DalList/VolunteerImplementation.cs
@@ -7,26 +7,44 @@ public class VolunteerImplementation : IVolunteer
 {
     public void Create(Volunteer item)
     {
-        throw new NotImplementedException();
+        if (Read(item.Id) != null)
+            throw new Exception("volunteer with this ID already exists");
+        DataSource.Volunteer.Add(item);
     }
+
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        var VolunteerToRemove = DataSource.Volunteer.Find(x => x.Id == id);
+        if (VolunteerToRemove != null)
+            DataSource.Volunteer.Remove(VolunteerToRemove);
+        else
+            throw new Exception("Volunteer with this ID does not exist.");
     }
+
     public void DeleteAll()
     {
-        throw new NotImplementedException();
+        DataSource.Volunteer.Clear();
     }
+
     public Volunteer? Read(int id)
     {
-        throw new NotImplementedException();
+        return DataSource.Volunteer.Find(x => x.Id == id);
     }
+
     public List<Volunteer> ReadAll()
     {
-        throw new NotImplementedException();
+        return new List<Volunteer>(DataSource.Volunteer);
     }
+
     public void Update(Volunteer item)
     {
-        throw new NotImplementedException();
+        var existingVolunteer = DataSource.Volunteer.Find(x => x.Id == item.Id);
+        if (existingVolunteer != null)
+        {
+            Delete(existingVolunteer.Id);
+            DataSource.Volunteer.Add(item);
+        }
+        else
+            throw new Exception("Volunteer with this ID does not exist.");
     }
 }
